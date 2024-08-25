8c9227a78dd835e6c79557864f214653b1cd027a
SUJP123 <sujaypatel04@gmail.com>
2024-08-25 02:42:34 -0400

    first commit

---
+++ src/main/java/com/collegeproject/gymappbackend/GymAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.collegeproject.gymappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppBackendApplication.class, args);
+	}
+
+}
